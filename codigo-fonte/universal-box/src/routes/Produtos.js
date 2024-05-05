8aeb67c026b62a441fb89426700e684848176f48
Mariana Mercier <marianammercier@gmail.com>
2024-05-04 22:20:18 -0300

    Altera layout da  tela de produtos

--- codigo-fonte/universal-box/src/routes/Produtos.js
+++ codigo-fonte/universal-box/src/routes/Produtos.js
@@ -1,59 +1,58 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect  } from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Navbar';
 
 function Produtos() {
-
-  const [returnedData, setReturnedData] = useState('teste use state');
   const [produtos, setProdutos] = useState([]);
 
-  const fetchData = async () => {
-    const newData = await fetch('/api', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api');
+        if (!response.ok) {
+          throw new Error('Erro ao obter os dados');
+        }
+        const data = await response.json();
+        setProdutos(data);
+      } catch (error) {
+        console.error('Erro:', error);
       }
-    })
-      .then(res => res.json());
-    console.log(newData);
-    setProdutos(newData)
-  }
+    };
 
-  useState(() => fetchData(), [])
+    fetchData();
+  }, []);
 
   return (
     <div>
       <Navbar />
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Nome</th>
-            <th scope="col">Fornecedor</th>
-            <th scope="col">Modelo</th>
-            <th scope="col">Preço</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            produtos.map((produto) => {
-              return (
-                <tr key={produto.ProdutoId} >
-                  <td>{produto.ProdutoId}</td>
-                  <td>{produto.ProdutoNome}</td>
-                  <td>{produto.FornecedorNome}</td>
-                  <td>{produto.ProdutoModelo}</td>
-                  <td>{produto.ProdutoPreco}</td>
-                </tr>
-              )
-            })
-          }
-        </tbody>
-        <Link to="/cadastroproduto" class="btn btn-primary">Cadastrar Novo Produto</Link>
-      </table>
+      <div className="container mt-5">
+        <h2>Lista de Produtos</h2>
+        <Link to="/cadastroproduto" className="btn btn-primary mb-3">Cadastrar Novo Produto</Link>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Nome</th>
+              <th scope="col">Fornecedor</th>
+              <th scope="col">Modelo</th>
+              <th scope="col">Preço</th>
+            </tr>
+          </thead>
+          <tbody>
+            {produtos.map((produto) => (
+              <tr key={produto.ProdutoId}>
+                <td>{produto.ProdutoId}</td>
+                <td>{produto.ProdutoNome}</td>
+                <td>{produto.FornecedorNome}</td>
+                <td>{produto.ProdutoModelo}</td>
+                <td>{produto.ProdutoPreco}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
